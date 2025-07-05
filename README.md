81f664ab8236e57b726f0afdea41ba83452656dd
Estiven Mejia <mejiaguetoestiven@gmail.com>
2025-07-04 23:50:55 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>Learn AI</h1>
+
+<span>By _Estiven Mejia_ and _David Mejia_</span>
---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+const App = () => {
+
+  return (
+    <>
+    <header>
+      <h1>Welcome to Learn AI</h1>
+    </header>
+      <main>
+        <p>This is a simple react App</p>
+      </main>
+      <footer>
+        This is the footer
+      </footer>
+    </>
+  )
+}
+
+export {
+  App
+}
